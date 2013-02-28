fcc86a51f06f5e9cb318e3d41e7c38fed77ca14d
Clément Vayer <cvayer@gmail.com>
2013-02-28 19:26:11 +0100

    Update Ogl flag in android project for textures

--- RubeLoaderTest-android/src/com/mangecailloux/rube/MainActivity.java
+++ RubeLoaderTest-android/src/com/mangecailloux/rube/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
         
         initialize(new RubeLoaderTest(), cfg);
     }
