561940400138b3d4263e435509367e961177a717
Clément Vayer <cvayer@gmail.com>
2013-03-11 14:06:50 +0100

    Added RubeSprite test, seems to work

---
+++ RubeLoader/src/com/badlogic/gdx/rube/RubeSprite.java
@@ -0,0 +1,93 @@
+package com.badlogic.gdx.rube;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class RubeSprite extends Sprite {
+
+	private final Vector2 tmp = new Vector2();
+	
+	private			Body	body;
+	private 		float	offsetAngle;
+	private final   Vector2 offsetPosition;
+	
+	/** Creates a sprite with width, height, and texture region equal to the size of the texture. */
+	public RubeSprite (Texture texture, RubeImage imageData) {
+		super(texture);
+		offsetPosition = new Vector2();
+		setupFromImage(imageData);
+	}
+
+	public RubeSprite (TextureRegion region, RubeImage imageData) {
+		super(region);
+		offsetPosition = new Vector2();
+		setupFromImage(imageData);
+	}
+	
+	public RubeSprite (RubeImage imageData) {
+		super(new Texture(imageData.file));
+		offsetPosition = new Vector2();
+		setupFromImage(imageData);
+	}
+	
+	void setupFromImage(RubeImage imageData)
+	{
+		flip(imageData.flip, false);
+		setColor(imageData.color);
+		setSize(imageData.width, imageData.height);
+		
+		float halfWidth = imageData.width / 2.0f;
+		float halfHeight = imageData.height /2.0f;
+		
+		setOrigin(halfWidth, halfHeight);
+		
+		body = imageData.body;
+		
+		offsetAngle = imageData.angle* MathUtils.radiansToDegrees;
+		offsetPosition.set(imageData.center);
+		
+		if(body != null)
+		{
+			float bodyAngle = body.getAngle()* MathUtils.radiansToDegrees;
+			tmp.set(offsetPosition).rotate(bodyAngle).add(body.getPosition()).sub(halfWidth, halfHeight);
+			setRotation(bodyAngle + offsetAngle);
+		}
+		else
+		{
+			tmp.set(imageData.center).sub(halfWidth, halfHeight);
+			setRotation(offsetAngle);
+		}
+		
+		setPosition(tmp.x, tmp.y);
+	}
+	
+	@Override
+	public void draw (SpriteBatch spriteBatch) {
+		updateBodyInfos();
+		super.draw(spriteBatch);
+	}
+
+	@Override
+	public void draw (SpriteBatch spriteBatch, float alphaModulation) {
+		
+		updateBodyInfos();
+		super.draw(spriteBatch, alphaModulation);
+	}
+	
+	void updateBodyInfos()
+	{
+		if(body != null)
+		{
+			float bodyAngle = body.getAngle() * MathUtils.radiansToDegrees;
+			tmp.set(offsetPosition).rotate(bodyAngle).add(body.getPosition()).sub(getWidth()/2.0f, getHeight()/2.0f);
+			setPosition(tmp.x, tmp.y);
+			setRotation(bodyAngle + offsetAngle);
+			
+		}	
+	}
+}
--- RubeLoaderTest/src/com/mangecailloux/rube/SpriteRenderer.java
+++ RubeLoaderTest/src/com/mangecailloux/rube/SpriteRenderer.java
@@ -1,29 +1,21 @@
 package com.mangecailloux.rube;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.rube.RubeImage;
+import com.badlogic.gdx.rube.RubeSprite;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ObjectMap;
 
 public class SpriteRenderer
 {
-
 	Array<Sprite> sprites;
-	ObjectMap<Sprite, Body> bodies;
-	ObjectMap<Sprite, RubeImage> images;
 	
 	final Vector2 tmp = new Vector2();
 	
 	public SpriteRenderer()
 	{
 		sprites = new Array<Sprite>();
-		bodies = new ObjectMap<Sprite, Body>();
-		images = new ObjectMap<Sprite, RubeImage>();
 	}
 	
 	public void addImages(Array<RubeImage> _images)
@@ -43,35 +35,10 @@ public class SpriteRenderer
 		{
 			RubeImage image = (RubeImage)_image;
 			
-			Texture texture = new Texture("data/" + image.file );
-			Sprite sprite = new Sprite(texture);
+			image.file = "data/"+image.file;
 			
-			sprite.flip(image.flip, false);
-			sprite.setColor(image.color);
-			sprite.setSize(image.width, image.height);
-			sprite.setOrigin(image.width /2, image.height /2);
-			
-			if(_image.body != null)
-			{
-				bodies.put(sprite, _image.body);
-			
-				
-				float bodyAngle = _image.body.getAngle()* MathUtils.radiansToDegrees;
-				
-				tmp.set(_image.center).rotate(bodyAngle).add(_image.body.getPosition()).sub(image.width/2, image.height/2);
-				
-				sprite.setRotation(bodyAngle + image.angle* MathUtils.radiansToDegrees);
-			}
-			else
-			{
-				tmp.set(_image.center).sub(image.width /2, image.height /2);
-				sprite.setRotation(image.angle* MathUtils.radiansToDegrees);
-			}
-			
-			sprite.setPosition(tmp.x, tmp.y);
-			
+			RubeSprite sprite = new RubeSprite(image);			
 			sprites.add(sprite);
-			images.put(sprite, image);
 		}
 	}
 	
@@ -79,20 +46,7 @@ public class SpriteRenderer
 	{
 		for(int i =0; i  < sprites.size; ++i)
 		{
-			Sprite sprite = sprites.get(i);
-			Body body = bodies.get(sprite);
-			RubeImage image = images.get(sprite);
-			
-			if(body != null)
-			{
-				float bodyAngle = body.getAngle() * MathUtils.radiansToDegrees;
-				tmp.set(image.center).rotate(bodyAngle).add(image.body.getPosition()).sub(image.width/2, image.height/2);
-				
-				sprite.setPosition(tmp.x, tmp.y);
-				sprite.setRotation(bodyAngle + image.angle* MathUtils.radiansToDegrees);
-				
-			}		
-			
+			Sprite sprite = sprites.get(i);			
 			sprite.draw(_batch);
 		}
 	}
